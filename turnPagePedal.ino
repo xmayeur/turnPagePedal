09946a277b2d19c58aecd29206b21740ddeb7e04
Xavier Mayeur <xavier@mayeur.be>
2019-01-04 23:15:51 +0100

    Code implemented as per functional specifications

--- turnPagePedal.ino
+++ turnPagePedal.ino
@@ -4,7 +4,7 @@
 #define leftBPpin 2
 #define rightBPpin 3
 #define LED 13
-#define DEBUG true
+#define durationStep 500
 
 void setup() {
   // initialize Serial:
@@ -17,76 +17,129 @@ void setup() {
   delay(200);
 }
 void loop() {
-  int leftBP, leftBPPrev; // left button state and previous state
-  int rightBP, rightBPPrev; // right button state and previous state
+  int leftBP; // left button state
+  int rightBP; // right button state
   long startLeft, endLeft; // time in ms - measure how long left button is maintained pressed
   long startRight, endRight; // same for right button
-  
-  leftBPPrev = HIGH;
-  rightBPPrev = HIGH;
+  int lcounter, rcounter;
+  String state, prevState;
+  long duration;
+  bool adminMode;
+
+  prevState = "HH";
+  lcounter = 0;
+  rcounter = 0;
+  adminMode = false;
+  duration = 1000;
 
   while (1)
   {
     // read both button state from digital inputs
-	leftBP = digitalRead(leftBPpin);
+    leftBP = digitalRead(leftBPpin);
     rightBP = digitalRead(rightBPpin);
 
-	/// detect right button state changes
-    if (rightBP == HIGH and rightBPPrev == LOW) // right button released
+    // establish current button state
+    if (leftBP == HIGH and rightBP == HIGH)
     {
-      endRight = millis(); // stop counting time
-      if (endRight - startRight > 500) // long press > 500 ms will send the 'c' character
+      state = "HH";
+    } else if (leftBP == HIGH and rightBP == LOW)
+    {
+      state = "HL";
+    } else if (leftBP == LOW and rightBP == HIGH)
+    {
+      state = "LH";
+    } else
+    {
+      state = "LL";
+    }
+
+    // manage button state changes
+    if (prevState == "HH" and state == "HL") // right button is pressed
+    {
+      startRight = millis();
+      digitalWrite(LED, HIGH);
+
+      if (adminMode) 
+      {
+        Serial.print("AT+ROLE=ROLE_PERIPHERAL") + Serial.println(); delay(100);
+        Serial.print("AT+FSM=FSM_HID_USB_COM_BLE_AT") + Serial.println(); delay(100);      
+        // blinkLED(250, 4);
+      }
+    }
+
+    if (prevState == "HH" and state == "LH") // left button is pressed
+    {
+      startRight = millis();
+      digitalWrite(LED, HIGH);
+      if (adminMode)
+      {
+        Serial.print("AT+ROLE=ROLE_CENTRAL") + Serial.println(); delay(100);
+        Serial.print("AT+FSM=FSM_TRANS_USB_COM_BLE") + Serial.println(); delay(100);
+        // blinkLED(500, 2);        
+      }
+    }
+
+    // right button is released alone
+    if (prevState == "HL" and state == "HH")
+    {
+      endRight = millis(); // stop right button timer
+      if (adminMode) // exit from admin mode if it was set
+      {
+        adminMode = false;
+      } else if (lcounter > 0) // manage long press parameter
+      {
+        duration = lcounter * durationStep;
+        lcounter = 0;
+      } else if (endRight - startRight > duration) // long press > duration ms will send the 'c' character
       {
         BLE.press_key(HID_KEYBOARD_C); delay(100);
         BLE.press_key(HID_KEYBOARD_RESERVED); delay(10);
-      } else
+      } else // right button short press - send a right cursor code
       {
         digitalWrite(LED, LOW);
         BLE.press_key(HID_KEYBOARD_RIGHT_ARROW); delay(100); // short press sends a right cursor code
         BLE.press_key(HID_KEYBOARD_RESERVED); delay(10);
       }
-    } else if (rightBP == LOW and rightBPPrev == HIGH) // button is pressed - start counting time
-    {
-      startRight = millis();
-      digitalWrite(LED, HIGH);
     }
 
-	// manage left button state
-    if (leftBP == HIGH and leftBPPrev == LOW) // left button is released
+    // left button is released alone
+    if (prevState == "LH" and state == "HH") // left button is released
     {
-      endLeft = millis();
-      if (endLeft - startLeft > 4000) // very long press >4s - try to set HID mode
+      endLeft = millis(); // stop left button timer
+      if (adminMode) // exit from admin mode if it was set
       {
-        Serial.print("AT+FSM=FSM_HID_USB_COM_BLE_AT") + Serial.println(); delay(10);
-        Serial.print("AT+ROLE=ROLE_PERIPHERAL") + Serial.println(); delay(10);
-
-        blinkLED(250, 4);
-      } else if (endLeft - startLeft > 2000) //  long press >2s & <4s - set UART transparent mode to program the Arduino processor
+        adminMode = false;
+      } else if (rcounter >= 3) // enter admin mode
       {
-        Serial.print("AT+FSM=FSM_TRANS_USB_COM_BLE") + Serial.println(); delay(10);
-        Serial.print("AT+ROLE=ROLE_CENTRAL") + Serial.println(); delay(10);
-        blinkLED(500, 2);
-      } else
+        adminMode = true;
+        rcounter = 0;
+      } else if (endRight - startRight > duration) // long press > duration ms  - nothing for now
       {
-	  // short press send a left cursor code
+        // do something later
+      } else // left button short press - send a left cursor code
+      {
         digitalWrite(LED, LOW);
-        BLE.press_key(HID_KEYBOARD_LEFT_ARROW); delay(100);
+        BLE.press_key(HID_KEYBOARD_LEFT_ARROW); delay(100); // short press sends a left cursor code
         BLE.press_key(HID_KEYBOARD_RESERVED); delay(10);
-
       }
-
-    } else if (leftBP == LOW and leftBPPrev == HIGH) // button is pressed - start measurng time
-    {
-      digitalWrite(LED, HIGH);
-      startLeft = millis();
-
     }
 
-	// memorize previous state
-    leftBPPrev = leftBP; // memorise previous state
-    rightBPPrev  = rightBP;
+    // right button is maintained down - left button is pressed
+    if (prevState == "HL" and state == "LL")
+    {
+      lcounter = (++lcounter) % 5; // increase counter for long press duration setting
+    }
+
+    // left button is maintained down - right button is pressed
+    if (prevState == "LH" and state == "LL")
+    {
+      rcounter += 1; // increase counter to pass in admin mode
+    }
+
+    // memorize previous state
+    prevState = state;
     delay(100);
-  }
+  } // end of while(1)
 }
 
 void blinkLED(int period, int nr)
